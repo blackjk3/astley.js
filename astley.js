257cc65cd8fcb6122df208af9d568c610a461fce
Aaron Johnson <juice.johnson@gmail.com>
2012-02-09 08:50:38 -0600

    We're no strangers to love

--- astley.js
+++ astley.js
@@ -1,18 +1,22 @@
 /*
  *
  * Authors: Jason Kadrmas (@itooamaneatguy)
+ *          Aaron Johnson (@ge3kusa)
  * Astley.js: You know the rules... And so do I!
  * License: MIT
  *
  *
  */
 
-(function() {
-	var tags = document.body.getElementsByTagName('a'),
-		url = 'http://youtu.be/dQw4w9WgXcQ';
-	
-	for(var i=0,len=tags.length; i<len; i++) {
-		tags[i].href = url;
-		tags[i].target = '_blank';
-	}
-})();
+(function () {
+    "use strict";
+    var tags = document.body.getElementsByTagName('a'),
+        url = 'http://youtu.be/dQw4w9WgXcQ',
+        i,
+        len;
+
+    for (i = 0, len = tags.length; i < len; i = i + 1) {
+        tags[i].href = url;
+        tags[i].target = '_blank';
+    }
+}());
